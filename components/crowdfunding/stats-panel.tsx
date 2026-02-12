1f94416bf220d09fc4842c75b431bcaccf920f5b
Lionel Yu <support@musicalbasics.com>
2026-02-12 02:03:04 -0800

    update reserve button text to 'reserve for batch 1'

--- components/crowdfunding/stats-panel.tsx
+++ components/crowdfunding/stats-panel.tsx
@@ -128,7 +128,7 @@ export function StatsPanel() {
         onClick={handleBackProject} // <--- Added click handler
         className="w-full bg-emerald-600 hover:bg-emerald-700 text-white font-semibold py-6 text-lg"
       >
-        Reserve for End of Year Batch
+        reserve for batch 1
       </Button>
 
       {/* No-Risk Guarantee */}
